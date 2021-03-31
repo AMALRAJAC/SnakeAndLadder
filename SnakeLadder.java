b665d96f9a86f8debe3ec0e15d6b36f48f6cb0a7
unknown <acamalraj@gmail.com>
2021-03-31 20:40:54 +0530

    add snake and ladder uc4

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -5,35 +5,44 @@ public class SnakeLadder {
 	    public static void main (String [] args)
 	    {
 	    	for (int i=0;temp<=100;i++) {
+	    		
 	    	System.out.println("/////enter 1 to play again////// ");
 	    	System.out.println("/////enter 0 to pause play ////// ");
+	    	
 	    	Scanner sc = new Scanner(System.in);
         	int check  = sc.nextInt();
 	        
 	        if ( check ==1) {
+	        	
 	        	//user input
 	        	int player1Position1 =(int) Math.floor(Math.random()*10)%6 +1 ;
 	        	System.out.println("dice number = "+player1Position1);
+	        	
 	        if (temp+player1Position1<=100) {
+	        	
 	        	temp = temp +player1Position1;
 	        	temp=snakeBit(temp);
 	        	temp=ladder(temp);
 	        	System.out.println("player position is: "+temp);
+	        	
 	        	//condition for checking winner
+	        	
 	        	if (temp==100) {
 	        		System.out.println("player won");
 	        		System.exit(0);
 	        	}
 	        }
-	        }if(check==0) {
+	        if(temp<0) {
+	        	// if number goes negative
+        		temp=0;
+        	}
+	        }
+	        if(check==0) {
+	        	// player want to pause play
 	        	System.out.println("player stays at "+temp);
 	        }
 	    	}
 	    }
-	    private static void exit(int i) {
-			// TODO Auto-generated method stub
-			
-		}
 		//defining snake bits in program
 	    private static int snakeBit(int temp) {
 			switch(temp) {
