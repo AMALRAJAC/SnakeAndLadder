89301d45f060c5b42d11e512e0cd66bb7aaed08c
AMAL RAJ AC <80584417+AMALRAJAC@users.noreply.github.com>
2021-04-01 08:55:18 +0530

    Update SnakeLadder.java

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -4,26 +4,22 @@ public class SnakeLadder {
 	
 	    public static void main (String [] args)
 	    {
-	    	
-	    	
-	        System.out.println("ENTER THE VALUE BETWEEN 1 TO 6....!");
+	    	for (int i=0;temp<=100;i++) {
+	        	
+	        				//user input
+	        				int player1Position1 =(int) Math.floor(Math.random()*10)%6 +1 ;
+	        				System.out.println("\t\t\t\tdice number of player1 = "+player1Position1);
+	        	
+	        				temp = temp +player1Position1;
+	        											
+	        								
+	        						
+	        						
+	        		}
 	        
-	        
-	        for (int i=0;temp<100;i++) {
-	        	//user input
-	        	int player1Position1 =(int) Math.floor(Math.random()*10)%6 +1 ;
-	        	System.out.println(player1Position1);
-	        if (player1Position1 <=6 && player1Position1>0 && temp+player1Position1<=100) {
-	        	temp = temp +player1Position1;
-	        	System.out.println("player position is"+temp);
-	        	//condition for checking winner
-	        	if (temp==100) {
-	        		System.out.println("player won");
-	        	}
-	        }
-	        
+	    	System.out.println("\n\n\t\t\t\t|||*****player1 won*******|||");	
+	    	}
 	    }
-	    }
-}
-	        
+	    
+		
 	        
