a2e19d11dbc3544cd8c64063dd8f459e81d63bcc
AMAL RAJ AC <80584417+AMALRAJAC@users.noreply.github.com>
2021-04-01 09:00:14 +0530

    Update SnakeLadder.java

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,29 +1,13 @@
-import java.util.Scanner;
+
 public class SnakeLadder {
-	public static int temp=0;
 	
 	    public static void main (String [] args)
 	    {
-	    	
-	    	
-	        System.out.println("ENTER THE VALUE BETWEEN 1 TO 6....!");
-	        
-	        
-	        for (int i=0;temp<100;i++) {
-	        	//user input
-	        	Scanner sc = new Scanner(System.in);
-	        	int player1Position1 = sc.nextInt();
-	        if (player1Position1 <=6 && player1Position1>0 && temp+player1Position1<=100) {
-	        	temp = temp +player1Position1;
-	        	System.out.println("player position is"+temp);
-	        	//condition for checking winner
-	        	if (temp==100) {
-	        		System.out.println("player won");
-	        	}
-	        }
-	        
+	     int playerPosition=0;   
+	    	System.out.println("player start at position= "+playerPosition);	
+	    	}
 	    }
-	    }
-}
+	    
+		
 	        
 	        
